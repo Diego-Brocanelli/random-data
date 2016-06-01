5924a9a394461f8335900d648b5a53ad7117ab28
Diego Brocanelli <diegod2@msn.com>
2016-05-31 21:52:34 -0300

    Implementation validation of CPF and CNPJ

--- src/Random/Generate/CPF/CPF.php
+++ src/Random/Generate/CPF/CPF.php
@@ -2,13 +2,12 @@
 
 namespace Random\Generate\CPF;
 
-use Random\Generate\CPF\Helper\RandomNumber;
-use Random\Generate\CPF\Helper\RestDivision;
+use Random\Generate\AbstractGenerator;
 
 /**
  * @author Diego Brocanelli <contato@diegobrocanelli.com.br>
  */
-class CPF
+class CPF extends AbstractGenerator
 {
     /**
      * Generate CPF valid.
@@ -17,35 +16,32 @@ class CPF
     public function generate()
     {
         $eleven = 11;
-        $randomNumber = new RandomNumber();
 
-        $n1 = $randomNumber->generate();    
-        $n2 = $randomNumber->generate();    
-        $n3 = $randomNumber->generate();    
-        $n4 = $randomNumber->generate();    
-        $n5 = $randomNumber->generate();    
-        $n6 = $randomNumber->generate();    
-        $n7 = $randomNumber->generate();    
-        $n8 = $randomNumber->generate();    
-        $n9 = $randomNumber->generate();    
-
-        $restDivision = new RestDivision();
+        $n1 = $this->randomNumber();    
+        $n2 = $this->randomNumber();    
+        $n3 = $this->randomNumber();    
+        $n4 = $this->randomNumber();    
+        $n5 = $this->randomNumber();    
+        $n6 = $this->randomNumber();    
+        $n7 = $this->randomNumber();    
+        $n8 = $this->randomNumber();    
+        $n9 = $this->randomNumber();    
 
         $sumD1 = ($n9*2) + ($n8*3) + ($n7*4) + ($n6*5) + ($n5*6) + ($n4*7) + ($n3*8) + ($n2*9) + ($n1*10);
-        $d1    = $eleven - ($restDivision->rest($sumD1, $eleven));
+        $d1    = $eleven - ( $this->mod( $sumD1, $eleven ) );
 
         if($d1 >= 10){
             $d1 = 0;
         }
 
         $sumD2 = ($d1*2) + ($n9*3) + ($n8*4) + ($n7*5) + ($n6*6) + ($n5*7) + ($n4*8) + ($n3*9) + ($n2*10) + ($n1*11);
-        $d2    = $eleven - ($restDivision->rest($sumD2, $eleven));
+        $d2    = $eleven - ( $this->mod( $sumD2, $eleven ) );
         
         if($d2 >= 10){
             $d2 = 0;
         }
         
-        $cpf = $n1.$n2.$n3.'.'.$n4.$n5.$n6.'.'.$n7.$n8.$n9.'.'.$d1.$d2;
+        $cpf = $n1.$n2.$n3.$n4.$n5.$n6.$n7.$n8.$n9.$d1.$d2;
 
         return $cpf;
     }
---
+++ src/Random/Validate/CNPJ/CNPJ.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace Random\Validate\CNPJ;
+
+/**
+ * @author Diego Brocanelli <contato@diegobrocanelli.com.br>
+ */
+class CNPJ
+{
+    /**
+     * Validate the CNPJ
+     * Code ported from https://github.com/Respect/Validation
+     * @param  string  $cnpj
+     * @return boolean      
+     */
+    public function isValid($cnpj)
+    {
+        //Code ported from jsfromhell.com
+        $cnpj = preg_replace('/\D/', '', $cnpj);
+        
+        $base = array(6, 5, 4, 3, 2, 9, 8, 7, 6, 5, 4, 3, 2);
+        
+        if ( strlen($cnpj) != 14 ) {
+            return false;
+        }
+        
+        for ( $interator = 0, $number = 0; $interator < 12; $number += $cnpj[$interator] * $base[++$interator] );
+        
+        if ( $cnpj[12] != ( ( ( $number %= 11 ) < 2) ? 0 : 11 - $number ) ) {
+            return false;
+        }
+        
+        for ( $interator = 0, $number = 0; $interator <= 12; $number += $cnpj[$interator] * $base[$interator++] );
+        
+        if ( $cnpj[13] != ( ( ( $number %= 11 ) < 2 ) ? 0 : 11 - $number ) ) {
+            return false;
+        }
+        
+        return true;
+    }
+}
