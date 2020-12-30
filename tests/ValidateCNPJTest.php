0466a3affbf42d9344b8bdb90bf0e3649f9287cc
Diego Brocanelli <diegod2@msn.com>
2020-12-30 16:52:20 -0300

    Realizado atualização dos códigos para adequação a nova syntaxe do PHP 7.4.

--- tests/ValidateCNPJTest.php
+++ tests/ValidateCNPJTest.php
@@ -1,47 +1,54 @@
 <?php
 
-use Random\Validate\CNPJ\CNPJ as ValidateCNPJ;
+declare(strict_types=1);
+
+namespace Random\Tests;
+
+use Random\Validate\CNPJ;
 use PHPUnit\Framework\TestCase;
 
 class ValidateCNPJTest extends TestCase
 {
-    private $CNPJ;
+    private $cnpj;
 
-    public function setUp()
+    public function setUp(): void
     {
-        $this->CNPJ = new ValidateCNPJ();
+        $this->cnpj = new CNPJ();
     }
 
-    public function testCNPJIsValid()
+    /** @test */
+    public function valid()
     {
-        $this->assertTrue( $this->CNPJ->isValid('67233081000140') );
-        $this->assertTrue( $this->CNPJ->isValid('15.925.474/0001-11') );
-        $this->assertTrue( $this->CNPJ->isValid('75193645000197') );
-        $this->assertTrue( $this->CNPJ->isValid(' 26.568.685/0001-60 ') );
-        $this->assertTrue( $this->CNPJ->isValid(' 79721575000153') );
-        $this->assertTrue( $this->CNPJ->isValid('13.212.735/0001-20 ') );
-        $this->assertTrue( $this->CNPJ->isValid('23473737000127') );
+        $this->assertTrue($this->cnpj->isValid('67233081000140'));
+        $this->assertTrue($this->cnpj->isValid('15.925.474/0001-11'));
+        $this->assertTrue($this->cnpj->isValid('75193645000197'));
+        $this->assertTrue($this->cnpj->isValid(' 26.568.685/0001-60 '));
+        $this->assertTrue($this->cnpj->isValid(' 79721575000153'));
+        $this->assertTrue($this->cnpj->isValid('13.212.735/0001-20 '));
+        $this->assertTrue($this->cnpj->isValid('23473737000127'));
     }
 
-    public function testEmptyCNPJ()
+    /** @test */
+    public function emptyCNPJ()
     {
-        $this->assertFalse( $this->CNPJ->isValid('') );
+        $this->assertFalse($this->cnpj->isValid(''));
     }
 
-    public function testInvalidCNPJ()
+    /** @test */
+    public function invalid()
     {
-        $this->assertFalse( $this->CNPJ->isValid('12345946415645asadas61654654') );
-        $this->assertFalse( $this->CNPJ->isValid('abclandsandlas2313sd13a') );
-        $this->assertFalse( $this->CNPJ->isValid('dsafadfadfasdf') );
-        $this->assertFalse( $this->CNPJ->isValid('444.555.999-11') );
-        $this->assertFalse( $this->CNPJ->isValid('1321231.561561.51561-106545') );
-        $this->assertFalse( $this->CNPJ->isValid('13.5.1-1') );
-        $this->assertFalse( $this->CNPJ->isValid('11.321.628/1000-05') );
-        $this->assertFalse( $this->CNPJ->isValid('69552514000122') );
-        $this->assertFalse( $this->CNPJ->isValid('03100063405418') );
-        $this->assertFalse( $this->CNPJ->isValid('@$%@$%@') );
-        $this->assertFalse( $this->CNPJ->isValid('00244099000140') );
-        $this->assertFalse( $this->CNPJ->isValid('99999999999999') );
-        $this->assertFalse( $this->CNPJ->isValid('23473737000128') );
+        $this->assertFalse($this->cnpj->isValid('12345946415645asadas61654654'));
+        $this->assertFalse($this->cnpj->isValid('abclandsandlas2313sd13a'));
+        $this->assertFalse($this->cnpj->isValid('dsafadfadfasdf'));
+        $this->assertFalse($this->cnpj->isValid('444.555.999-11'));
+        $this->assertFalse($this->cnpj->isValid('1321231.561561.51561-106545'));
+        $this->assertFalse($this->cnpj->isValid('13.5.1-1'));
+        $this->assertFalse($this->cnpj->isValid('11.321.628/1000-05'));
+        $this->assertFalse($this->cnpj->isValid('69552514000122'));
+        $this->assertFalse($this->cnpj->isValid('03100063405418'));
+        $this->assertFalse($this->cnpj->isValid('@$%@$%@'));
+        $this->assertFalse($this->cnpj->isValid('00244099000140'));
+        $this->assertFalse($this->cnpj->isValid('99999999999999'));
+        $this->assertFalse($this->cnpj->isValid('23473737000128'));
     }
 }
