7bd689c788547bf3284c96cd442767dbeaae64d2
Diego Brocanelli <diegod2@msn.com>
2020-12-31 08:50:29 -0300

    Alterado assinatura do método para melhorar a legibilidade do output dos testes.

--- tests/ValidateCNPJTest.php
+++ tests/ValidateCNPJTest.php
@@ -29,7 +29,7 @@ class ValidateCNPJTest extends TestCase
     }
 
     /** @test */
-    public function emptyCNPJ()
+    public function emptyCnpj()
     {
         $this->assertFalse($this->cnpj->isValid(''));
     }
